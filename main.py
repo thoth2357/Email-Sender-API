fbf6b11bb86a78b121eb844284670edf04211dbf
Thoth <98170427+thoth2357@users.noreply.github.com>
2022-06-26 17:21:39 +0100

    cops

--- main.py
+++ main.py
@@ -1,15 +1,7 @@
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
+
 import Route
 
 app = FastAPI()
-origins = [
-    "*"
-]
-app.add_middleware(
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["POST"],
-    allow_headers=["*"],
-)
+
 app.include_router(Route.router)
